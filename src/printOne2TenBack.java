b379261d665e4a543ae81793f31035711005467c
sw333zy@gmail.com <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-01-31 12:51:59 -0500

    Initial commit

---
+++ src/printOne2TenBack.java
@@ -0,0 +1,8 @@
+public class printOne2TenBack {
+    public static void main(String[] args){
+        // Index is set at 10. As long as the index is greater than 0, take away 1. Then print
+        for (int i = 10; i > 0; --i){
+            System.out.println(i);
+        }
+    }
+}
